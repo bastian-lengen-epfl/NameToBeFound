324c3e93dc500828ac4b917e83e67865e373fadd
bastian-lengen-epfl <bastian.lengen@epfl.ch>
2022-02-25 10:20:02 +0100

    update

--- Fit_parameters.py
+++ Fit_parameters.py
@@ -1,13 +1,15 @@
 ### Cepheids
-include_Cepheids = False
+include_Cepheids = True
 N_galaxies_Cep = 19         # Number of SN-host galaxies
 N_anchors_Cep = 2           # Number of anchors
 include_MW = True           # Considers the MW-Cepheids and their GAIA parallax
 fixed_zp = False            # False -> Fit for the GAIA zp
 zp = 0.14                   # GAIA zp in μas, ONLY IF fixed_zp = True
+sig_zp = 0.05		    # zp uncertainty, ONLY IF fixed_zp = True
 fixed_Zw = False            # False -> Fit for the metallicity effect on the PLR
 Zw = 0                      # Slope of the metallicity effect on the PLR ONLY IF fixed_Zw = True
-PLR_break = False            # Allows for a break in the PLR at P=10d
+sig_Zw = 0		    # Zw uncertainty, ONLY IF fixed_Zw = True
+PLR_break = False           # Allows for a break in the PLR at P=10d
 break_P = 10                # Period of the PLR-break ONLY IF PLR_break = True
 added_scatter = 0.0682      # Add dispersion to the Cepheids (0.0682 in Moertsell et al. 2021)
 outlier_rejection = True    # 2.7-clipping process
@@ -16,8 +18,8 @@ Kcorr_Cep = False           # Correct for the K-corrections (Anderson 2021)
 
 
 ### TRGB
-include_TRGB = True
-N_galaxies_TRGB = 15        # Number of SN-host galaxies
+include_TRGB = False
+N_galaxies_TRGB = 12        # Number of SN-host galaxies
 N_anchors_TRGB = 1          # Number of anchors
 Kcorr_TRGB = False          # Correct for the K-corrections (Anderson 2021)
 use_color = True            # Consider or not the color (V-I) of the TRGB (like Anand 2021)
@@ -33,7 +35,7 @@ fit_aB = True
 a_B = 0.715840              # Value for the Hubble diagram's intercept ONLY IF fit_aB = False
 sig_a_B = 0.001631          # 0.71273 +/- 0.00176 Riess 2016, OR can be fitted
 z_min = 0.023               # Min redshift to consider when fitting for a_B
-z_max = 0.15               # Max redshift to consider when fitting for a_B
+z_max = 0.150               # Max redshift to consider when fitting for a_B
 ### Wesenheit
 R = 0.386
 
